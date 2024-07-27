6559f425a1ca761a319327e14ac9b71f020f5b48
Pedro-V <mingas62@hotmail.com>
2024-07-27 17:09:08 -0300

    Updates to build and entry point

---
+++ src/caju/Main.java
@@ -0,0 +1,38 @@
+package caju;
+import caju.lexer.*;
+import caju.node.*;
+import java.io.*;
+
+public class Main
+{
+	public static void main(String[] args)
+	{
+    String arquivo = "";
+		try {
+			arquivo = args[0];
+    } catch (ArrayIndexOutOfBoundsException ex) {
+      System.err.println("Usage: caju <input>.caju");
+      System.exit(1);
+    }
+
+    Lexer lexer = null;
+    try {
+      lexer = new Lexer(new PushbackReader(new FileReader(arquivo), 1024)); 
+    } catch (IOException ex) {
+			System.err.println(ex.getMessage());
+      System.exit(1);
+    }
+
+    try {
+      Token token;
+      while(!((token = lexer.next()) instanceof EOF)) {
+        System.out.println(token.getClass());
+        System.out.println(" ( "+token.toString()+")");
+      }
+    } catch (Exception ex) {
+      System.err.println(ex.getMessage());
+      System.exit(1);
+    }
+	}
+}
+
