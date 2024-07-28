32acef3f37a496a39c050a62998975127d2f26bd
Pedro-V <mingas62@hotmail.com>
2024-07-28 14:49:43 -0300

    Build para após erro no meio do processo

--- build.sh
+++ build.sh
@@ -10,9 +10,9 @@ LEXER_DAT="$PACKAGE/lexer/lexer.dat"
 rm -rf ${SRC_DIR}/${PACKAGE}/{analysis,lexer,node,parser} $TARGET_DIR
 
 # Build target.
-java -jar sablecc.jar ${SRC_DIR}/*.sable
-javac $(find ${SRC_DIR}/ -name '*.java') -d ${TARGET_DIR}
-cp ${SRC_DIR}/${LEXER_DAT} ${TARGET_DIR}/${LEXER_DAT}
+java -jar sablecc.jar ${SRC_DIR}/*.sable &&
+  javac $(find ${SRC_DIR}/ -name '*.java') -d ${TARGET_DIR} &&
+  cp ${SRC_DIR}/${LEXER_DAT} ${TARGET_DIR}/${LEXER_DAT}
 
 # Uncomment for running.
 #java -cp ${TARGET_DIR} $PACKAGE.Main
